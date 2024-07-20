cfcd0d1ebb873eeb11164cef8f5f54a3e001c7b8
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-20 12:10:14 +0530

    minor changes

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,4 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://hpyhycuhxoqdjzkpbpag.supabase.co';
-  static const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhweWh5Y3VoeG9xZGp6a3BicGFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0NTY4MDYsImV4cCI6MjAzNzAzMjgwNn0.8iD4-qKDWnOT2Fpd2FH9ukAYiusjyy-sPdVMaoTwdsc';
-}
