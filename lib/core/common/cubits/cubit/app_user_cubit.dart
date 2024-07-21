c230cb48e49dd9b5139ce2527cbb38363d9b27e3
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-21 23:34:01 +0530

    adding cubit

---
+++ lib/core/common/cubits/cubit/app_user_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'app_user_state.dart';
+
+class AppUserCubit extends Cubit<AppUserState> {
+  AppUserCubit() : super(AppUserInitial());
+}
---
+++ lib/core/common/cubits/cubit/app_user_state.dart
@@ -0,0 +1,6 @@
+part of 'app_user_cubit.dart';
+
+@immutable
+sealed class AppUserState {}
+
+final class AppUserInitial extends AppUserState {}
