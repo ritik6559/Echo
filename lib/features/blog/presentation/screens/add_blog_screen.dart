74dd4dd8ead9d4d1ca6f0ed7a59bcb4880f6b4e6
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-22 23:59:04 +0530

    picking images from gallery

--- lib/features/blog/presentation/screens/add_blog_screen.dart
+++ lib/features/blog/presentation/screens/add_blog_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/uitls/pick_image.dart';
 import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +16,18 @@ class AddBlogScreen extends StatefulWidget {
 class _AddBlogScreenState extends State<AddBlogScreen> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
+  File? image;
 
   List<String> selectedTopics = [];
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -41,36 +54,56 @@ class _AddBlogScreenState extends State<AddBlogScreen> {
           padding: const EdgeInsets.all(12.0),
           child: Column(
             children: [
-              DottedBorder(
-                radius: const Radius.circular(40),
-                color: AppPallete.borderColor,
-                dashPattern: const [15, 6],
-                strokeCap: StrokeCap.round,
-                child: Container(
-                  height: 150,
-                  width: double.infinity,
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text(
-                        'Select your image',
-                        style: TextStyle(
-                          fontSize: 15,
+              image != null
+                  ? GestureDetector(
+                    onTap: selectImage,
+                    child: SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                  )
+                  : GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: DottedBorder(
+                        radius: const Radius.circular(20),
+                        color: AppPallete.borderColor,
+                        dashPattern: const [15, 6],
+                        strokeCap: StrokeCap.round,
+                        child: Container(
+                          height: 150,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                'Select your image',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
               const SizedBox(height: 20),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
