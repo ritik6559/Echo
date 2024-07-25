3b816cd8e4ca9b2e3d5f170d589b8b66c4a90d01
Ritik6559 <ritikjoshi741@gmail.com>
2024-07-25 23:06:54 +0530

    completing blog details screen

--- lib/features/blog/presentation/screens/blog_details_screen.dart
+++ lib/features/blog/presentation/screens/blog_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/core/uitls/calculate_reading_time.dart';
+import 'package:blog_app/core/uitls/format_date.dart';
 import 'package:blog_app/features/blog/domain/entities/blog.dart';
 import 'package:flutter/material.dart';
 
@@ -14,40 +15,57 @@ class BlogDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              blog.title,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  blog.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'By ${blog.posterName}',
+                  style:
+                      const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  '${formatDateBydMMMYYYY(blog.updatedAt)} • ${calculateReadingTime(blog.content)} min',
+                  style: const TextStyle(
+                    color: AppPallete.greyColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: double.infinity,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      fit: BoxFit.cover,
+                      blog.imageUrl,
+                    ),
+                  ),
+                ),
+                Text(
+                  blog.content,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    height: 2,
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            Text(
-              'By ${blog.posterName}',
-              style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              '${blog.updatedAt} . ${calculateReadingTime(blog.content)} min',
-              style: const TextStyle(
-                color: AppPallete.greyColor,
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(height: 20),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(
-                blog.imageUrl,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
